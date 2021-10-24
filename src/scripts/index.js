d2f1137958e50c2d0e8acb8288464d2809fb2015
Ilya Zhigarev <zhigarev@bk.ru>
2021-10-25 01:02:33 +0300

    release 1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -17,10 +17,11 @@ let loaderTimeout;
  * Функция задаёт первоначальное состояние страницы.
  * Отправляется первый запрос за картинками, юез параметров т.к. с дефолтными настройками.
  */
-const initialState = function() {
-  action.disabled = false;
-  getPictures();
-  // console.log(action)
+const initialState = function () {
+    action.disabled = false;
+    getPictures();
+    // renderPictures()
+    // console.log("action", action)
 };
 
 /**
@@ -29,16 +30,16 @@ const initialState = function() {
  * @param {number} page
  * @param {number} limit
  */
-const getPictures = function(page = 1, limit = 10) {
-  showLoader();
-  fetch(`https://picsum.photos/v2/list?page=${page};limit=${limit}`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(result) {
-      renderPictures(result);
-    });
-  hideLoader();
+const getPictures = function (page = 1, limit = 10) {
+    // showLoader();
+    fetch(`https://picsum.photos/v2/list?page=${page};limit=${limit}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            renderPictures(result);
+        });
+    hideLoader();
 };
 
 /**
@@ -46,34 +47,34 @@ const getPictures = function(page = 1, limit = 10) {
  * и вызывает ф-цию для отрисовки картинки в попапе
  * @param {number} id
  */
-const getPictureInfo = function(id = 0) {
-  showLoader();
-  fetch(`https://picsum.photos/id/${id}/info`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(result) {
-      renderPopupPicture(result);
-    });
+const getPictureInfo = function (id = 0) {
+    showLoader();
+    fetch(`https://picsum.photos/id/${id}/info`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            renderPopupPicture(result);
+        });
 };
 
 /**
  * Функция показывает индикатор загрузки.
- * Меняет ситили, ничего не возвращает.
+ * Меняет стили, ничего не возвращает.
  */
-const showLoader = function() {
-  loader.style.visibility = "visible";
+const showLoader = function () {
+    loader.style.visibility = "visible";
 };
 
 /**
  * Функция скрывает индикатор загрузки.
  * Удаляет таймаут индикатора, ничего не возвращает.
  */
-const hideLoader = function() {
-  loaderTimeout = setTimeout(function() {
-    loader.style.visibility = "hidden";
-    loaderTimeout.clearTimeout();
-  }, 700);
+const hideLoader = function () {
+    loaderTimeout = setTimeout(function () {
+        loader.style.visibility = "hidden";
+        loaderTimeout.clearTimeout();
+    }, 700);
 };
 
 /**
@@ -83,12 +84,12 @@ const hideLoader = function() {
  * @param {string} src
  * @param {number} size
  */
-const cropImage = function(src, size = 2) {
-  const [domain, key, id, width, height] = src.split("/").splice(2);
-  const newWidth = Math.floor(+width / size);
-  const newHeight = Math.floor(+height / size);
+const cropImage = function (src, size = 2) {
+    const [domain, key, id, width, height] = src.split("/").splice(2);
+    const newWidth = Math.floor(+width / size);
+    const newHeight = Math.floor(+height / size);
 
-  return `https://${domain}/${key}/${id}/${newWidth}/${newHeight}`;
+    return `https://${domain}/${key}/${id}/${newWidth}/${newHeight}`;
 };
 
 /**
@@ -96,30 +97,32 @@ const cropImage = function(src, size = 2) {
  * заполняет его и встраивает в разметку
  * @param {array} list
  */
-const renderPictures = function(list) {
-  if (!list.length) {
-    throw Error(`Pictures not defined. The list length: ${list.length}`);
-  }
-  console.log(list)
-  const clone = templateImageCard.content.cloneNode(true);
-  console.log("clone", clone)
-  const fragment = document.createDocumentFragment();
-
-  list.forEach(function(element) {
-    const link = clone.querySelector("a");
-    console.log(element)
-    link.href = element.url;
-    link.dataset.id = element.id;
-
-    const image = clone.querySelector("img");
-    image.src = cropImage(element.download_url, 5);
-    image.alt = element.author;
-    image.classList.add("preview");
-    fragment.appendChild(clone);
-  });
-
-  container.appendChild(fragment);
-  hideLoader();
+const renderPictures = function (list) {
+    if (!list.length) {
+        throw Error(`Pictures not defined. The list length: ${list.length}`);
+    }
+    // let total = 0
+    // total = total + list.length
+    // console.log(total);
+    // console.log("clone", clone);
+    // console.log("fragment", fragment);
+    // console.log("container¡", container);
+    list.forEach(function (element) {
+        const clone = templateImageCard.content.cloneNode(true);
+        const fragment = document.createDocumentFragment();
+        // console.log(list)
+        const link = clone.querySelector("a");
+        // console.log("link", link);
+        link.href = element.url;
+        link.dataset.id = element.id;
+        const image = clone.querySelector("img");
+        image.src = cropImage(element.download_url, 5);
+        image.alt = element.author;
+        image.classList.add("preview");
+        fragment.appendChild(clone);
+        container.appendChild(fragment);
+        // console.log("+++");
+    });
 };
 
 /**
@@ -127,29 +130,32 @@ const renderPictures = function(list) {
  * заполняет его и встраивает в попап
  * @param {object} picture
  */
-const renderPopupPicture = function(picture) {
-  const clone = templateImagePopup.content.cloneNode(true);
-  const img = clone.querySelector("img");
-  const link = clone.querySelector("a");
-  const author = clone.querySelector(".author");
+const renderPopupPicture = function (picture) {
+    // console.log(picture)
+    popupContainer.innerHTML = "";
+    const clone = templateImagePopup.content.cloneNode(true);
+    const img = clone.querySelector("img");
+    const link = clone.querySelector("a");
+    const author = clone.querySelector(".author");
 
-  img.src = cropImage(picture.download_url, 2);
-  img.alt = picture.author;
-  author.textContent = picture.author;
-  img.width = picture.width / 10;
-  link.href = picture.download_url;
+    img.src = cropImage(picture.download_url, 2);
+    img.alt = picture.author;
+    author.textContent = picture.author;
+    img.width = picture.width / 10;
+    // img.height = picture.height / 10;
+    link.href = picture.download_url;
 
-  popupContainer.innerHTML = "";
-  popupContainer.appendChild(clone);
-  hideLoader();
-  togglePopup();
+    // console.log("+++")
+    popupContainer.appendChild(clone);
+    hideLoader();
+    togglePopup();
 };
 
 /**
- * Функция переклбчает класс открытия на попапе
+ * Функция переключает класс открытия на попапе
  */
-const togglePopup = function() {
-  popup.classList.toggle("open");
+const togglePopup = function () {
+    popup.classList.toggle("open");
 };
 
 /**
@@ -160,19 +166,20 @@ const togglePopup = function() {
  * Обработчик кнопки подгрузки картинок
  * @param {MouseEvent} evt
  */
-const actionHandler = function(evt) {
-  evt.preventDefault();
-  const nextPage = evt.currentTarget.dataset.page;
-  evt.currentTarget.dataset.page = nextPage + 1;
+const actionHandler = function (evt) {
+    evt.preventDefault();
+    const nextPage = evt.currentTarget.dataset.page;
+    // console.log(nextPage)
+    evt.currentTarget.dataset.page = +nextPage + 1;
 
-  if (nextPage > MAX_PAGE_IAMGES) {
-    console.warn(
-      `WARN: You are trying to call a page that exceeds ${MAX_PAGE_IAMGES}`
-    );
-    evt.currentTarget.disabled = true;
-  } else {
-    getPictures(nextPage);
-  }
+    if (nextPage > MAX_PAGE_IAMGES) {
+        console.warn(
+            `WARN: You are trying to call a page that exceeds ${MAX_PAGE_IAMGES}`
+        );
+        evt.currentTarget.disabled = true;
+    } else {
+        getPictures(nextPage);
+    }
 };
 
 /**
@@ -181,12 +188,13 @@ const actionHandler = function(evt) {
  * для открытия попапа с ней
  * @param {MouseEvent} evt
  */
-const imageHandler = function(evt) {
-  evt.preventDefault();
-
-  if (evt.target.closest("a")) {
-    getPictureInfo(evt.target.dataset.id);
-  }
+const imageHandler = function (evt) {
+    evt.preventDefault();
+    // console.log(evt.target.closest("a"))
+    if (evt.target.closest("a")) {
+        // console.log("11", evt.target.closest("a").dataset.id)
+        getPictureInfo(evt.target.closest("a").dataset.id);
+    }
 };
 
 action.addEventListener("click", actionHandler);
